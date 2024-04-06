c3d0a32f869bd42a32ad0578f15de499486de1f1
Sarah E <seatherly.prsvr@gmail.com>
2024-04-05 20:13:41 -0500

    html

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+
 
 //For FCC testing purposes
 fccTestingRoutes(app);
